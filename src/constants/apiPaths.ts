c988d76ac8d60ac48659523cc4a135b0b490e416
Dmitrii Sysoev <dmitrii_sysoev1@epam.com>
2022-08-22 00:23:51 +0400

    task 8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	import: 'https://qf0w72sxti.execute-api.eu-west-1.amazonaws.com/dev/import',
 	bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	cart: 'http://xdmus-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
